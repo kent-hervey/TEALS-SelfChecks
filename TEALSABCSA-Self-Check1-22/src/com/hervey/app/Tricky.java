04648f5e9bcd99a6762dfccf94e3496d0d5ff236
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-09-09 16:17:32 -0500

    first commit for Self Checks

---
+++ TEALSABCSA-Self-Check1-22/src/com/hervey/app/Tricky.java
@@ -0,0 +1,23 @@
+package com.hervey.app;
+
+public class Tricky {
+
+	public static void main(String[] args) {
+		message1();
+		message2();
+
+	}
+
+	private static void message1() {
+		System.out.println("This is message1.");
+
+	}
+
+	private static void message2() {
+		System.out.println("This is message2.");
+		message1();
+		System.out.println("Done with message2");
+
+	}
+
+}
