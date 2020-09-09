85e54e3b5b2104577077f550f81a5925758cdfba
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-09-09 18:00:18 -0500

    added 1-26

---
+++ TEALSABCSA-Self-Check1-26/src/com/hervey/app/Confusing.java
@@ -0,0 +1,30 @@
+package com.hervey.app;
+
+public class Confusing {
+
+	private static void method2() {
+		method1();
+		System.out.println("I am method 2.");
+
+	}
+
+	private static void method3() {
+		method2();
+		System.out.println("I am method 3");
+		method1();
+
+	}
+
+	private static void method1() {
+		System.out.println("I am method 1");
+	}
+
+	public static void main(String[] args) {
+		method1();
+		method3();
+		method2();
+		method3();
+
+	}
+
+}
